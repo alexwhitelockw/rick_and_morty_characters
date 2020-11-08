4ea3487cfa371f9c2045647d9f464629552cf78a
alexwhitelockw <alexwhitelockw@gmail.com>
2020-11-08 13:48:05 +1100

    Added missing semi-colons

--- apiCall.js
+++ apiCall.js
@@ -3,49 +3,49 @@ function getRndInt (min, max) {
 }
 async function characterRandomiser () {
 
-    let rndInt = await getRndInt(1, 20)
+    let rndInt = await getRndInt(1, 20);
 
     let initialUrl = new URL("https://rickandmortyapi.com/api/character/");
-    initialUrl.searchParams.set("page", rndInt)
+    initialUrl.searchParams.set("page", rndInt);
 
     let cardArea = document.getElementById("cardArea");
 
-    let fetchCall = await fetch(initialUrl.href)
+    let fetchCall = await fetch(initialUrl.href);
 
-    let fetchJson = await fetchCall.json()
+    let fetchJson = await fetchCall.json();
 
-    let fetchResults = await fetchJson.results
+    let fetchResults = await fetchJson.results;
 
     fetchResults.map(x => {  // Fetch results is an array therefore mapping across each element
 
-        let characterCard = document.createElement("div")
-        characterCard.className = "card"
-        characterCard.setAttribute("data-aos", "fade-up")
-        characterCard.setAttribute("data-aos-duration", '2000')
-        characterCard.innerHTML = `<img src=${x.image}>`
+        let characterCard = document.createElement("div");
+        characterCard.className = "card";
+        characterCard.setAttribute("data-aos", "fade-up");
+        characterCard.setAttribute("data-aos-duration", '2000');
+        characterCard.innerHTML = `<img src=${x.image}>`;
 
-        let characterDetails = document.createElement("div")
-        characterDetails.className = "details"
+        let characterDetails = document.createElement("div");
+        characterDetails.className = "details";
         characterDetails.innerHTML = `<h4>${x.name}</h4>
-                                      <h4>${x.species}</h4>`
+                                      <h4>${x.species}</h4>`;
 
         if (x.status == 'Alive') {
-            characterDetails.innerHTML += `<h5> <i class="fas fa-circle" id="aliveCircle"></i>  ${x.status}</h5>`
+            characterDetails.innerHTML += `<h5> <i class="fas fa-circle" id="aliveCircle"></i>  ${x.status}</h5>`;
         }
         else if (x.status == 'Dead') {
-            characterDetails.innerHTML += `<h5> <i class="fas fa-circle" id="deadCircle"></i>  ${x.status}</h5>`
+            characterDetails.innerHTML += `<h5> <i class="fas fa-circle" id="deadCircle"></i>  ${x.status}</h5>`;
         }
         else {
-            characterDetails.innerHTML += `<h5> <i class="fas fa-circle" id="unknownCircle"></i>  ${x.status}</h5>`
+            characterDetails.innerHTML += `<h5> <i class="fas fa-circle" id="unknownCircle"></i>  ${x.status}</h5>`;
         }
 
-        characterCard.appendChild(characterDetails)
-        cardArea.appendChild(characterCard)
+        characterCard.appendChild(characterDetails);
+        cardArea.appendChild(characterCard);
 
     })
 }
 
 
-window.addEventListener("load", () => characterRandomiser())
+window.addEventListener("load", () => characterRandomiser());
 
-document.getElementById("randomCharacter").addEventListener("click", () => characterRandomiser())
+document.getElementById("randomCharacter").addEventListener("click", () => characterRandomiser());
